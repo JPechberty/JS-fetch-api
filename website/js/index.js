6bafd823e8feb0eae0b2ba24b75de63eb87d4cd8
JPechberty <j.pechberty@gmail.com>
2024-01-31 23:54:13 +0100

    Add async function support example

--- website/js/index.js
+++ website/js/index.js
@@ -1,25 +1,42 @@
 console.log("index.js loaded")
 
 
-fetch('../json/content.json')
-    .then(function (response) {
-        console.log(response);
-        return response.json();
-    })
-    .then(function (data) {
-        console.log(data);
+// fetch('../json/content.json')
+//     .then(function (response) {
+//         console.log(response);
+//         return response.json();
+//     })
+//     .then(function (data) {
+//         console.log(data);
+//         const contentGrid = document.querySelector('#content-grid');
+//         data.forEach(obj=>buildCard(obj, contentGrid))
+//         document.querySelector('#info-alert').classList.add('d-none');
+//         document.querySelector('#content-grid').classList.remove('d-none');
+//         console.log("request succeeded");
+//     })
+//     .catch(function (error) {
+//         console.error(error);
+//         document.querySelector('#info-alert').classList.add('d-none');
+//         document.querySelector('#error-alert').classList.remove('d-none');
+//     });
+
+document.addEventListener('DOMContentLoaded', async function () {
+    let content = [];
+
+    try {
+        content = await getContent();
+
         const contentGrid = document.querySelector('#content-grid');
-        data.forEach(obj=>buildCard(obj, contentGrid))
+        content.forEach(obj=>buildCard(obj, contentGrid))
         document.querySelector('#info-alert').classList.add('d-none');
         document.querySelector('#content-grid').classList.remove('d-none');
-        console.log("request succeeded");
-    })
-    .catch(function (error) {
+
+    }catch(error){
         console.error(error);
         document.querySelector('#info-alert').classList.add('d-none');
         document.querySelector('#error-alert').classList.remove('d-none');
-    });
-
+    }
+});
 
 function buildCard(obj, containerElement){
     const template = document.querySelector('#content-card-template');
@@ -28,4 +45,9 @@ function buildCard(obj, containerElement){
     clone.querySelector(".card-text").textContent = obj.content;
     clone.querySelector(".stretched-link").href = obj.link;
     containerElement.append(clone);
+}
+
+async function getContent(){
+    const response = await fetch('../json/content.json');
+    return await response.json();
 }
